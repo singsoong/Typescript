3ff9604210dea7cd5601c0a726528d4b2295745a
Singsoong <o__hyeah@naver.com>
2022-06-20 23:13:22 +0900

    Typescript 기초(2), (3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import React, { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" borderColor="red"/>
-    </>
+    <Container>
+      <H1>hello world</H1>
+    </Container>
   );
 }
 
