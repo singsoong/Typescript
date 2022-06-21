a477c127605e05b7482754c03ced0d5cc8d266c5
Singsoong <o__hyeah@naver.com>
2022-06-21 22:51:26 +0900

    Router, Font, CSS 기본설정

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
+function Coin() {
+  const { coinId } = useParams();
+  return <Title>{coinId}</Title>;
+}
+
+export default Coin;
---
+++ src/routes/Router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Coin from "./Coin";
+import Coins from "./Coins";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Coins />}></Route>
+        <Route path="/:coinId" element={<Coin />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
